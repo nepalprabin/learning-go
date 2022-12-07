db59b31bb115a198380dbc33d4e4845404b1cd25
Prabin Nepal <prabinnepal1996@gmail.com>
2022-12-07 11:20:43 -0600

    Code Refactoring

--- hello_test.go
+++ hello_test.go
@@ -5,10 +5,25 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		if got != want {
+			t.Errorf("got, %q, want, %q", got, want)
+		}
+	})
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got, %q, want, %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
